ac3502903aecfb1e8df4072df74325967bc78127
hoanganzglobal <hoanganzglobal@gmail.com>
2021-04-27 15:51:09 +0700

    Update clientSocket.js

--- public/js/clientSocket.js
+++ public/js/clientSocket.js
@@ -6,6 +6,8 @@ var socketOptions = {
 };
 
 var statusCodeToString = {
+  10: 'Pending',
+  12: 'First Order',
   15: 'POS Received',
   18: 'Cooking',
   20: 'Cooked',
@@ -13,6 +15,12 @@ var statusCodeToString = {
   28: 'Delivered',
   30: 'Completed',
   40: 'Void',
+};
+
+var syncStatus = {
+  113: 'Not Push',
+  200: 'Pushing',
+  202: 'Pushed',
 };
 
 var socket = io(SOCKET_URL, socketOptions);
