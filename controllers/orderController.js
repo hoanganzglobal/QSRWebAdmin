9a7cb638020503797888e17a47799eb316935754
hoanganzglobal <hoanganzglobal@gmail.com>
2021-04-29 21:05:40 +0700

    Update orderController.js

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
 const router = express.Router();
+const orderService = require('../services/orderService');
 
-var numbro = require("numbro");
+router.post('/', getOrders);
 
 router.get('/', (req, res, next) => {
   var data = [];
@@ -18,3 +19,19 @@ router.get('/', (req, res, next) => {
 });
 
 module.exports = router;
+
+function getOrders(req, res, next) {
+  var userLoggedIn = req.session.user ? req.session.user : {};
+  var t = new Promise((resolve, reject) => {
+    orderService
+      .getOrders(userLoggedIn)
+      .then((payload) => {
+        resolve(payload);
+      })
+      .catch((err) => next(err));
+  });
+
+  return t.then((payload) => {
+    return res.status(200).json(payload);
+  });
+}
